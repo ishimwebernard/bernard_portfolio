9f436c2ef07d242eaab966ba769a153f9357a51b
ishimwebernard <71332125+ishimwebernard@users.noreply.github.com>
2020-10-30 14:37:30 +0200

    Finalising the page

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function REVEALDOM(){
+    var myDoc = document.getElementById(`mavigationBarMobile`);
+    if(myDoc.style.display == `none`){
+        myDoc.style.display = `block`;
+    }else{
+        myDoc.style.display = `none`;
+    }}
