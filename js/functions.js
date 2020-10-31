d6dd98fe33a0f69aa2b672f960cd14c845a47a00
ishimwebernard <71332125+ishimwebernard@users.noreply.github.com>
2020-10-31 12:12:06 +0200

    Initial Commit

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function REVEALDOM(){
+    var myDoc = document.getElementById(`mavigationBarMobile`);
+    if(myDoc.style.display == `none`){
+        myDoc.style.display = `block`;
+    }else{
+        myDoc.style.display = `none`;
+    }
+                            
+}
