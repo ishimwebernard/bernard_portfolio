f495eafe8257d210c50753b9bca8ff72219c22a0
ishimwebernard <71332125+ishimwebernard@users.noreply.github.com>
2020-11-06 15:14:41 +0200

    Adding the form validation

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,23 @@
+
+function checkInput(){
+    var inputName = document.getElementById("name");
+    var inputPhone = document.getElementById("phonenumber");
+    var inputEmail = document.getElementById("email");
+    var inputMessage = document.getElementById("message");
+    var button = document.querySelector("#submit");
+    var reqEx = /\S+@\S+\.\S+/;
+
+    if(inputName.value.trim() != "" && reqEx.test(inputEmail.value) && inputPhone.value !== "" && inputMessage.value !== ""){
+        button.style.background = "#D70046";
+        button.disabled = false;
+    }else{
+        button.style.background = "#515151";
+        button.disabled = true;
+    }
+    console.log( `Input recorder`);
+}
+
+
 function REVEALDOM(){
     var myDoc = document.getElementById(`mavigationBarMobile`);
     if(myDoc.style.display == `none`){
