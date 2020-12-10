bb87d5d015ef00894edb267ae03f2b02ad3fb2bf
ishimwebernard <71332125+ishimwebernard@users.noreply.github.com>
2020-12-10 06:09:55 +0200

    #001 FT-API-PATCH-POST
    *. Added a method to update one post

---
+++ Server/controllers/postsController.js
@@ -0,0 +1,55 @@
+
+const Posts = require('../models/Posts')
+
+class Post{
+     static async savePost(request, response){
+        const toPost = new Posts({
+            title: request.body.title,
+            description: request.body.description,
+            imagesource: request.body.imagesource
+        });
+        console.log(request);
+        await toPost.save();
+        response.json(toPost);  }  
+
+
+        static retrieveAllPosts =  function(request, response){
+            console.log(`Retrieving Comments ...`);
+            Posts.find().then(data=>{
+                response.json(data);
+                console.log("Data Succesfully retrieved");
+            }).catch(error=>{
+                console.log(error);
+                response.send("Something went wrong");
+            })
+        }
+        static getOne = function(request,response){
+            Posts.findById(request.params.postId).then(data=>{
+                response.json(data);
+            }).catch(error=>{
+                console.log(error);
+                response.send(`Something went wrong`);
+            })
+        }
+        static deletePost = (request, response)=>{
+            Posts.remove({_id: request.params.postId}).then((data)=>{
+                console.log(`Deleted ${data}`);
+                response.json(data);
+            }).catch(error=>{
+                console.log(error);
+                response.send(`Something went wrong`);
+            })
+        }
+        static patchPost = (request, response)=>{
+            Posts.update({_id: request.params.postId},
+                { $set: {title: request.body.title}}
+                ).then((data)=>{
+                    response.json(data);
+                }).catch(error=>{
+                    console.log(error);
+                    response.send(`Something went wrong`);
+                })
+        
+        }
+}
+module.exports = Post;
