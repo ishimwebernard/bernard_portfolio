c6945016b2c43be833123b7ec1985bbc2178dae9
“ishimwebernard” <isbernard2001@gmail.com>
2020-12-11 13:58:42 +0200

    #001 FT-PATCH-COMMENT
    *.Initial Commit

---
+++ Server/routes/api/comments.js
@@ -0,0 +1,12 @@
+const commentsController = require('../../controllers/commentsController');
+const express = require('express');
+const router = express.Router();
+
+router.post("/", commentsController.saveComment);
+router.delete("/:commentId", commentsController.deleteThisComment);
+router.get("/", commentsController.retrieveComment);
+
+module.exports = router;
+
+
+
