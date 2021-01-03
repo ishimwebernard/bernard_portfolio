07d2d76000138c899bcf95708da7f160a2760dca
“ishimwebernard” <isbernard2001@gmail.com>
2021-01-03 16:31:51 +0200

    Adding Server files

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -3,13 +3,13 @@ require("dotenv/config");
 
 const connectoToMongo = function(){
     
-    mongoose.connect(process.env.MONGO_HASH,
+    mongoose.connect("mongodb+srv://bianca:bianca@cluster0.zgi7m.mongodb.net/sampledb?retryWrites=true&w=majority",
 
         { useUnifiedTopology: true,useNewUrlParser: true }).then(()=>{
             console.log("Server Connected to DB");
         
         }).catch(error=>{
-        
+            console.log(error)
         });
 }
 module.exports = connectoToMongo;
