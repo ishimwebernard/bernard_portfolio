8fbf0e9e3a41258efe3203fa9099cfdcebdf7e04
“ishimwebernard” <isbernard2001@gmail.com>
2021-01-07 11:14:03 +0200

    Removing Unwanted Code

--- helpers/joivalidator.js
+++ helpers/joivalidator.js
@@ -12,27 +12,10 @@ const userSignIn = Joi.object({
     .min(3).required(),
     email: Joi.string()
         .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()});
-
-const userLogin = Joi.object({
-    password: Joi.string()
-    .min(3).required(),
-email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-})
-
 class JoiValidate{
     static validateForSignUp = async function(signupUser){
         try{
             await userSignIn.validateAsync(signupUser);
-            return 'Success';
-        }catch(error){
-            return String(error.details[0].message);
-        }
-    }
-
-    static validateForLogin = async function(loginUser){
-        try{
-            await userLogin.validateAsync(loginUser);
             return 'Success';
         }catch(error){
             return String(error.details[0].message);
